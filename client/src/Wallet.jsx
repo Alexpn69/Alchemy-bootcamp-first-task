b45f6f8f13a8e9b2a78c53db2ec9c17d421de30a
Alex <alexpvn69@gmail.com>
2023-04-13 23:29:54 +0300

    2

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -24,6 +24,18 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       </label>
 
       <div className="balance">Balance: {balance}</div>
+      <p>Below you can find addresses for test and their initial balances</p>
+      <ul>
+  <li>
+    "0x2d954be28042d8767836c754a06747a9718706b1": 100
+  </li>
+  <li>
+  "0x67796ee2b751666b92c0a398bc087384dffefc21": 50
+  </li>
+  <li>
+  "0xd33a60ef2d625da2553698c5cf1136263d0f3724": 75
+  </li>
+      </ul>
     </div>
   );
 }
